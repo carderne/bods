6d803fff2806fd0f1bfa6a851ed61018b4c4052b
Chris Arderne <chris@rdrn.me>
2022-11-06 15:22:52 +0000

    Replace bus direction UI

--- docs/main.js
+++ docs/main.js
@@ -17,29 +17,63 @@ const map = new mapboxgl.Map({
   center: [-1.2, 51.75],
   zoom: 10,
   minZoom: 4,
-  maxZoom: 16,
+  maxZoom: 22,
   maxBounds: [-9, 45, 9, 65],
   projection: "globe",
   hash: "loc",
 });
 map.addControl(new mapboxgl.NavigationControl());
 
+const deg2rad = (deg) => deg * (Math.PI / 180);
+
 const emptyFc = () => ({ type: "FeatureCollection", features: [] });
 
 const addToMap = (r) => {
   const fc = emptyFc();
-  fc.features = r.map((f) => ({
-    type: "Feature",
-    properties: {
-      bearing: f.bearing,
-      route_id: f.route_id,
-    },
-    geometry: {
-      type: "Point",
-      coordinates: [f.lon, f.lat],
-    },
-  }));
+  fc.features = r
+    .map((f) => [
+      {
+        type: "Feature",
+        properties: {
+          bearing: f.bearing,
+          route_id: f.route_id,
+          which: 2,
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [f.lon, f.lat],
+        },
+      },
+      {
+        type: "Feature",
+        properties: {
+          which: 1,
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [
+            f.lon - 0.00005 * Math.sin(deg2rad(f.bearing)),
+            f.lat - 0.00005 * Math.cos(deg2rad(f.bearing)),
+          ],
+        },
+      },
+      {
+        type: "Feature",
+        properties: {
+          which: 0,
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [
+            f.lon - 0.00009 * Math.sin(deg2rad(f.bearing)),
+            f.lat - 0.00009 * Math.cos(deg2rad(f.bearing)),
+          ],
+        },
+      },
+    ])
+    .flat();
   map.getSource("bus").setData(fc);
+
   get("loading").style.display = "none";
 };
 
@@ -60,58 +94,60 @@ const getData = () => {
     });
 };
 
-const layout = {
-  "text-field": ["to-string", ["get", "route_id"]],
-  "text-size": 10,
-  "text-offset": [0.7, -0.7],
-  "icon-image": "arrow",
-  "icon-size": ["interpolate", ["linear"], ["zoom"], 8, 0.2, 16, 1.1],
-  "icon-rotate": [
-    "interpolate",
-    ["linear"],
-    ["get", "bearing"],
-    0,
-    0,
-    359,
-    359,
-  ],
-};
-
-const opacity = [
-  "interpolate",
-  ["linear"],
-  ["zoom"],
-  busMinZoom - 1,
-  0,
-  busMinZoom,
-  1,
-];
-
-const paint = {
-  "text-halo-color": "#ffffff",
-  "text-halo-width": 2,
-  "text-halo-blur": 1,
-  "icon-opacity": opacity,
-  "text-opacity": opacity,
-  "icon-color": "hsla(120, 76%, 40%, 1)",
-};
-
 map.on("load", () => {
   map.addSource("bus", {
     type: "geojson",
     data: emptyFc(),
   });
-
-  const img = new Image(24, 24);
-  img.src = "./arrow.svg";
-  img.onload = () => map.addImage("arrow", img, { sdf: true });
+  map.addSource("trail", {
+    type: "geojson",
+    data: emptyFc(),
+  });
+  map.addSource("trail2", {
+    type: "geojson",
+    data: emptyFc(),
+  });
 
   map.addLayer({
     id: "bus",
+    type: "circle",
+    source: "bus",
+    paint: {
+      "circle-stroke-color": "hsla(120, 76%, 40%, 1)",
+      "circle-radius": ["step", ["get", "which"], 2, 1, 4, 2, 5],
+      "circle-stroke-width": ["step", ["get", "which"], 1, 1, 2, 2, 3],
+      "circle-opacity": 0,
+      "circle-stroke-opacity": [
+        "interpolate",
+        ["linear"],
+        ["zoom"],
+        15,
+        ["case", ["==", ["get", "which"], 2], 1, 0],
+        22,
+        1,
+      ],
+    },
+  });
+
+  map.addLayer({
+    id: "bus-text",
     type: "symbol",
     source: "bus",
-    layout,
-    paint,
+    layout: {
+      "text-field": ["to-string", ["get", "route_id"]],
+      "text-size": ["interpolate", ["linear"], ["zoom"], 13, 10, 14, 12],
+      "text-offset": [0.7, -0.7],
+    },
+    paint: {
+      "text-halo-color": "#ffffff",
+      "text-halo-width": 2,
+      "text-halo-blur": 1,
+      "text-opacity": ["interpolate", ["linear"], ["zoom"], 13, 0, 14, 1],
+    },
+  });
+
+  map.on("click", "bus", (e) => {
+    console.log(e.features[0].properties.bearing);
   });
 
   getData();
